a85c525f279349ebecd162ac671c487d051f0812
Daniel Zenkovitch <daniel.zenkovitch@hotmail.com>
2020-06-27 19:59:07 -0700

    Updates

---
+++ precache-manifest.ce66f546b6acda2fecfd1b31aa141ed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59eca1d71cde56cf569e03189c16eaca",
+    "url": "/resume-website/index.html"
+  },
+  {
+    "revision": "62651c7354c76660bae0",
+    "url": "/resume-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "32ab3aea277fff39b78d",
+    "url": "/resume-website/static/js/2.67c15f34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume-website/static/js/2.67c15f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62651c7354c76660bae0",
+    "url": "/resume-website/static/js/main.1ba4c512.chunk.js"
+  },
+  {
+    "revision": "c4c7419b7edca97c7079",
+    "url": "/resume-website/static/js/runtime-main.0d2b2e5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
